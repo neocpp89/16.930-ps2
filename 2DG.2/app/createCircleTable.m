34c585d90b07c1f699e8ba2c0229d680b23f530d
Sachith Dunatunga <neocpp@gmail.com>
2015-03-26 23:20:19 -0400

    Better version of latex table generator.

--- 2DG.2/app/createCircleTable.m
+++ 2DG.2/app/createCircleTable.m
@@ -8,7 +8,9 @@ for i=1:numel(plist)
 end
 
 % Table for areas by volume integral
-fprintf(f, '\\begin{table}\n\\centering\\begin{tabular}{c |');
+fprintf(f, '\\begin{table}\n');
+fprintf(f, '\\caption{Here we show the results for the volume integral over the circular mesh.}\n');
+fprintf(f, '\\centering\\begin{tabular}{c |');
 for j=1:numel(sizelist)
     fprintf(f, ' c');
 end
@@ -20,7 +22,7 @@ fprintf(f, '\\\\\n');
 for i=1:numel(plist) 
     fprintf(f, 'p = %d ', plist(i));
     for j=1:numel(sizelist)
-        fprintf(f, '& %g ', area1(i, j));
+        fprintf(f, '& %10.10g ', area1(i, j));
     end
     fprintf(f, '\\\\\n');
 end
@@ -28,7 +30,9 @@ fprintf(f, '\\end{tabular}\n');
 fprintf(f, '\\label{tbl:area1}\n\\end{table}\n\n');
 
 % Table for areas by divergence and surface integral
-fprintf(f, '\\begin{table}\n\\centering\\begin{tabular}{c |');
+fprintf(f, '\\begin{table}\n');
+fprintf(f, '\\caption{Here we show the results for the surface integral of a divergence over the circular mesh.}\n');
+fprintf(f, '\\centering\\begin{tabular}{c |');
 for j=1:numel(sizelist)
     fprintf(f, ' c');
 end
@@ -40,7 +44,7 @@ fprintf(f, '\\\\\n');
 for i=1:numel(plist) 
     fprintf(f, 'p = %d ', plist(i));
     for j=1:numel(sizelist)
-        fprintf(f, '& %g ', area2(i, j));
+        fprintf(f, '& %10.10g ', area2(i, j));
     end
     fprintf(f, '\\\\\n');
 end
@@ -48,7 +52,9 @@ fprintf(f, '\\end{tabular}\n');
 fprintf(f, '\\label{tbl:area2}\n\\end{table}\n\n');
 
 % Table for perimeters
-fprintf(f, '\\begin{table}\n\\centering\\begin{tabular}{c |');
+fprintf(f, '\\begin{table}\n');
+fprintf(f, '\\caption{Here we show the results for the surface integral for the boundary over the circular mesh; again, something seems off with the cubic case.}\n');
+fprintf(f, '\\centering\\begin{tabular}{c |');
 for j=1:numel(sizelist)
     fprintf(f, ' c');
 end
@@ -60,7 +66,7 @@ fprintf(f, '\\\\\n');
 for i=1:numel(plist) 
     fprintf(f, 'p = %d ', plist(i));
     for j=1:numel(sizelist)
-        fprintf(f, '& %g ', perimeter(i, j));
+        fprintf(f, '& %10.10g ', perimeter(i, j));
     end
     fprintf(f, '\\\\\n');
 end
@@ -68,7 +74,9 @@ fprintf(f, '\\end{tabular}\n');
 fprintf(f, '\\label{tbl:perimeter}\n\\end{table}\n\n');
 
 % Table for areas by volume integral (diff)
-fprintf(f, '\\begin{table}\n\\centering\\begin{tabular}{c |');
+fprintf(f, '\\begin{table}\n');
+fprintf(f, '\\caption{Here we show the difference of the actual result ($\\pi$) and the volume integral over the circular mesh.}\n');
+fprintf(f, '\\centering\\begin{tabular}{c |');
 for j=1:numel(sizelist)
     fprintf(f, ' c');
 end
@@ -80,7 +88,7 @@ fprintf(f, '\\\\\n');
 for i=1:numel(plist) 
     fprintf(f, 'p = %d ', plist(i));
     for j=1:numel(sizelist)
-        fprintf(f, '& %g ', pi-area1(i, j));
+        fprintf(f, '& %10.10g ', pi-area1(i, j));
     end
     fprintf(f, '\\\\\n');
 end
@@ -88,7 +96,9 @@ fprintf(f, '\\end{tabular}\n');
 fprintf(f, '\\label{tbl:diffarea1}\n\\end{table}\n\n');
 
 % Table for areas by divergence and surface integral (diff)
-fprintf(f, '\\begin{table}\n\\centering\\begin{tabular}{c |');
+fprintf(f, '\\begin{table}\n');
+fprintf(f, '\\caption{Here we show the difference of the actual result ($\\pi$) and the surface integral of a divergence over the circular mesh.}\n');
+fprintf(f, '\\centering\\begin{tabular}{c |');
 for j=1:numel(sizelist)
     fprintf(f, ' c');
 end
@@ -100,7 +110,7 @@ fprintf(f, '\\\\\n');
 for i=1:numel(plist) 
     fprintf(f, 'p = %d ', plist(i));
     for j=1:numel(sizelist)
-        fprintf(f, '& %g ', pi-area2(i, j));
+        fprintf(f, '& %10.10g ', pi-area2(i, j));
     end
     fprintf(f, '\\\\\n');
 end
@@ -108,7 +118,9 @@ fprintf(f, '\\end{tabular}\n');
 fprintf(f, '\\label{tbl:diffarea2}\n\\end{table}\n\n');
 
 % Table for perimeters (diff)
-fprintf(f, '\\begin{table}\n\\centering\\begin{tabular}{c |');
+fprintf(f, '\\begin{table}\n');
+fprintf(f, '\\caption{Here we show the difference of the actual result ($2\\pi$) and the surface integral of the boundary over the circular mesh.}\n');
+fprintf(f, '\\centering\\begin{tabular}{c |');
 for j=1:numel(sizelist)
     fprintf(f, ' c');
 end
@@ -120,7 +132,7 @@ fprintf(f, '\\\\\n');
 for i=1:numel(plist) 
     fprintf(f, 'p = %d ', plist(i));
     for j=1:numel(sizelist)
-        fprintf(f, '& %g ', 2*pi-perimeter(i, j));
+        fprintf(f, '& %10.10g ', 2*pi-perimeter(i, j));
     end
     fprintf(f, '\\\\\n');
 end
