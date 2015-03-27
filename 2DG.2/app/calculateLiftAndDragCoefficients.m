d636ec0def3b72d33ff1ce6be6cd37e31253acf9
Sachith Dunatunga <neocpp@gmail.com>
2015-03-27 16:23:34 -0400

    Fixed bug with degree-radian mismatch.

--- 2DG.2/app/calculateLiftAndDragCoefficients.m
+++ 2DG.2/app/calculateLiftAndDragCoefficients.m
@@ -5,13 +5,15 @@ function [cl,cd,cl_analytic,alphas] = calculateLiftAndDragCoefficients(alphas, m
     master = mkmaster(mesh);
 
     for i=1:numel(alphas)
-        [psi, vel_a, gamma_a] = getAirfoilData(alphas(i), mesh);
+        alpha = alphas(i);
+        [psi, vel_a, gamma_a] = getAirfoilData(alpha, mesh);
         vel = calculateVelFromPsi(mesh, master, psi);
         cf = calculateForceCoefficientFromVel(mesh, master, vel);
-        vinf_dir = [cos(alphas(i)), sin(alphas(i))];
-        vperp_dir = [-sin(alphas(i)), cos(alphas(i))];
+        alpha_rad = alpha*pi/180.0;
+        vinf_dir = [cos(alpha_rad), sin(alpha_rad)];
+        vperp_dir = [-sin(alpha_rad), cos(alpha_rad)];
         cd(i) = norm(cf.*vinf_dir);
         cl(i) = norm(cf.*vperp_dir);
-        cl_analytic(:) = -0.5*gamma_a;
+        cl_analytic(i) = -0.5*gamma_a;
     end
 end
